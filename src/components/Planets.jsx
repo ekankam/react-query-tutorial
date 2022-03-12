a3255ddbccbcd599b7e1b628c6164606b276b284
ekankam <eb.kankam26@gmail.com>
2022-03-11 18:33:27 -0600

    Added a pagination functionality

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,29 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from 'react-query'
 import Planet from './Planet'
 
-const Planets = () => {
-  const fetchPlanets = async () => {
-    const response = await fetch('http://swapi.dev/api/planets/')
-    const data = await response.json()
-    return data
-  }
+const fetchPlanets = async (page = 0) => {
+  const response = await fetch(`http://swapi.dev/api/planets/?page=${page}`)
+  const data = await response.json()
+  return data
+}
 
-  const { data, status } = useQuery('planets', fetchPlanets)
+const Planets = () => {
+  const [page, setPage] = useState(1)
+  const { data, isLoading, isError, error, isFetched, isFetching } = useQuery(
+    ['planets', page],
+    () => fetchPlanets(page),
+    {
+      keepPreviousData: true,
+    }
+  )
+
+  const prevButtonHandler = () => setPage((page) => page - 1)
+  const nextButtonHandler = () => setPage((page) => page + 1)
+  const lastPage = data?.count / 10
 
   return (
     <div>
       <h2>Planets</h2>
 
-      {status === 'loading' && <h2>Loading data...</h2>}
-      {status === 'error' && <h4>Error fetching data</h4>}
-      {status === 'success' && (
+      <button
+        onClick={() => prevButtonHandler()}
+        disabled={page === 1}
+        className={`${page === 1 && 'disable'}`}
+      >
+        Previous Page
+      </button>
+      <span>Page: {page}</span>
+      <button
+        onClick={() => nextButtonHandler()}
+        disabled={page === lastPage}
+        className={`${page === lastPage && 'disable'}`}
+      >
+        Next Page
+      </button>
+
+      {isLoading && <h2>Loading data...</h2>}
+      {isError && <h4>{error}</h4>}
+      {isFetched && (
         <>
           {data.results?.map((planet) => (
             <Planet key={planet.name} planet={planet} />
           ))}
         </>
       )}
+      {isFetching && <h4>Loading next page...</h4>}
     </div>
   )
 }
